d72586faebb661d31638ce2152da53b93c385a8b
nvbach91 <nvbach91@outlook.com>
2016-06-29 11:58:57 +0200

    added port scanning, and disconnect detection to reinitialize port

--- app.js
+++ app.js
@@ -1,41 +1,109 @@
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("COM6", {
-    baudrate: 9600,
-    dataBits: 8,
-    stopBits: 1,
-    parity: 'none'
-});
 
-serialPort.on('open', function () {
-    var express = require('express');
-    var app = express();
-    var bodyParser = require('body-parser')
+/* EXAMPLE TO CALL THIS API
+ 
+ $.ajax({
+ url: 'http://localhost:2112/customerdisplay',
+ dataType: 'jsonp', jsonp: 'callback', data: {msg: 'Nguyen Hoai Nam\n42.00 Kc'},
+ success: function(resp){console.log(resp)}
+ });
+ 
+ */
 
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(bodyParser.json())
-    
-    app.get('/customerdisplay', function (req, res) {
-        var msg = req.query.msg;
-        msg = msg.replace("\n", "\x0a\x0d");
+//var dateFormat = require('dateformat');
+var SERIALPORT = require('serialport');
+var SerialPort = require('serialport').SerialPort;
+var serialPort = null;
+function initSerialPort() {
+    if (serialPort !== null) {
+        //console.log(dateFormat(new Date(), 'isoDateTime') + ' Active: ' + serialPort.path);
+    } else {
+        console.log('Scanning for serial ports');
+        SERIALPORT.list(function (err, ports) {
+            var comPort = '';
+            for (var i = 0; i < ports.length; i++) {
+                var port = ports[i];
+                if (port.manufacturer === 'Silicon Labs') {
+                    comPort = port.comName;
+                    break;
+                }
+            }
+            if (comPort !== '') {
+                console.log('Active ' + comPort + ': Silicon Labs ');
+
+                serialPort = new SerialPort(comPort, {
+                    baudrate: 9600,
+                    dataBits: 8,
+                    stopBits: 1,
+                    parity: 'none'
+                });
+                serialPort.on('close', function () {
+                    console.log('\tPort ' + comPort + ' closed');
+                    serialPort = null;
+                });
+                serialPort.on('disconnect', function () {
+                    console.log('\tPort ' + comPort + ' disconnected');
+                    serialPort.close();
+                });
+            } else {
+                console.log('\tNo serial ports are connected');
+            }
+
+        });
+    }
+    setTimeout(initSerialPort, 2000);
+}
+;
+function convertMessage(msg) {
+    var res = '';
+    for (var i = 0; i < msg.length; i++) {
+        res += convertCharacter(msg[i]);
+    }
+    return res;
+}
+;
+function convertCharacter(c) {
+    // \x0a\x0d = newline (line feed + carriage return = cursor down 1 line and go to far left
+    if (c === '\n') {
+        return '\x0a\x0d';
+    }
+    var diacritics = 'ěéĚÉšŠčČřŘžŽýÝáÁíÍóÓďĎťŤňŇúůÚŮ'; // czech diacritics
+    var nondiacrit = 'eeEEsScCrRzZyYaAiIoOdDtTnNuuUU';
+    for (var i = 0; i < diacritics.length; i++) {
+        if (c === diacritics[i]) {
+            return nondiacrit[i];
+        }
+    }
+    // return regular abc
+    return c;
+}
+
+initSerialPort();
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/customerdisplay', function (req, res) {
+    var msg = req.query.msg;
+    msg = convertMessage(msg);
+
+    // \x0c = clear display, reset cursor to first position
+    if (serialPort && serialPort.isOpen()) {
         serialPort.write('\x0c' + msg, function (err, bytesWritten) {
             if (err) {
-                console.log('ERROR: ', err.message);
-                res.jsonp({success: false, msg: err.messaage})
+                res.jsonp({success: false, msg: err.message});
             } else {
-                res.jsonp({success: true, msg: bytesWritten})
+                res.jsonp({success: true, msg: msg});
             }
         });
-    });
-    app.listen(2112, function () {
-        console.log('Open app http://localhost:2112')
-    });
-});
-/*
-
-$.ajax({
-  url: "http://localhost:2112/customerdisplay",
-  dataType: "jsonp", jsonp: "callback", data: {msg: "Nguyen Hoai Nam\n42.00 Kc"},
-  success: function(resp){console.log(resp)}
+    } else {
+        res.jsonp({success: false, msg: 'Customer display is not connected'});
+    }
 });
 
-*/
+app.listen(2112, function () {
+    console.log('Listening for custopmer display requests on http://localhost:2112');
+});
